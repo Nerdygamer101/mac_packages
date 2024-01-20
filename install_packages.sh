652597ce7394fcd972e588e4fe0d80148a03accd
Keanen <kramer2012@gmail.com>
2024-01-20 04:42:18 -0800

    Cleaned up the script

--- install_packages.sh
+++ install_packages.sh
@@ -11,19 +11,6 @@ else
   brew update
 fi
 
-# List of packages to install with Homebrew
-# 2023-07-27 - Added microsoft-teams
-# 2023-10-02 - Made the package names more explicit
-packages=(
-homebrew/cask/zoom
-homebrew/cask/adobe-acrobat-reader
-homebrew/cask/google-drive
-homebrew/cask/google-chrome
-homebrew/cask/firefox
-homebrew/cask/microsoft-teams
-  # Add more packages to install here
-)
-
 
 # Concatenate contents of all .package files and append to $packages.
 # Added so that we can have department or user specific package lists.
@@ -35,18 +22,7 @@ done
 
 # Install the packages using Homebrew
 echo "Installing packages..."
+echo "List of packages: $packages"
 brew install $packages
 
 echo "All packages installed successfully."
-
-#for package in "${packages[@]}"; do
-  # 2023-10-2 Removed the upgrade command for each package, the install command does this automatically
-  #if brew list "$package" &>/dev/null; then
-	# 2023-07-27 - Changed behavior from skipping the package to updating it instead
-    #echo "$package is already installed. Skipping..."
-	#brew upgrade "$package"
-  #else
-    #brew install $packages
-    #brew upgrade
-  #fi
-#done
